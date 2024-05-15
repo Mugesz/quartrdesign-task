bd512c522eb195d24eee197392c21710062f773c
frekerzzzz <kannamugesh135@gmail.com>
2024-05-15 14:06:46 +0530

    tt

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Logo from "../asserts/Logo.png";
 import long from "../asserts/Escultures.png";
-import line from "../asserts/img3.png"
+import Line from"../asserts/img3.png"
 
 function Footer() {
   return (
@@ -9,20 +9,20 @@ function Footer() {
       <div className="container">
         <div className="bg-footer-voilet position-relative">
           <div className="long">
-            <img src={long} alt="Your Logo" style={{ maxHeight: "180px" }} />
-           <div className="all">
-           <p>Thanks for visiting my website</p>
-            <p>
+            <img src={long} alt="Your Logo" style={{ maxHeight: "100px" }} />
+           
+            <p className="text-center">Thanks for visiting my website</p>
+         
+            <p className="text-center">
               If you have any questions, you can write to me on any of my social
               networks. I'm sure I'll answer you.
-            </p>
-           </div>
-           <img src={line} alt="" height="80px" />
+            </p> 
+          <img src={Line} alt="" height="50px" />
           </div>
         </div>
         <div className="d-flex justify-content-between align-items-center">
-          <div className="col-md-3 ">
-            <img src={Logo} alt="" />
+          <div className="col-md-3 d-flex align-items-center ">
+            <img src={Logo} alt="" className="img-fluid logo-2" />
             <div className="mt-3 footerflex">
               <p>
                 <a href="/" className="btn-footer text-white">
