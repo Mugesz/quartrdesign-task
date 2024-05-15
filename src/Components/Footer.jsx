e520225b1a80475994c64924a842cd884a1e488e
frekerzzzz <kannamugesh135@gmail.com>
2024-05-15 15:39:11 +0530

    fgvr

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Logo from "../asserts/Logo.png";
 import long from "../asserts/Escultures.png";
 import Line from "../asserts/img3.png";
+import "styles.css"
 
 function Footer() {
   return (
